0f3a958a09047ade1da64c251b220c9cdb4def40
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2025-04-01 10:58:23 -0700

    Convert replit to repo

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hello World JS
+
+This is an example Node project for students to practice an initial "Hello World" JavaScript program.
+
+1. Before starting, run `npm install` or `npm i` to install dependencies.
+
+2. Open up `src/index.js` to see the "Hello World" code.
+
+3. Use `npm start` to run the `src/index.js` file.
---
+++ src/index.js
@@ -0,0 +1,94 @@
+const axios = require('axios');
+
+axios
+  .get('https://dog.ceo/api/breeds/list/all')
+  .then(() => {
+    console.log('success!');
+  })
+  .catch(() => {
+    console.log('error!');
+  });
+
+
+//////////////////////////////////////////
+// Examining response and error objects //
+//////////////////////////////////////////
+
+// axios
+//   .get('https://dog.ceo/api/breeds/list/all')
+//   .then((response) => {
+//     console.log('The value of response is:', response);
+
+//     console.log('The value of status inside of response is:', response.status);
+
+//     console.log(
+//       'The date inside header inside response is:',
+//       response.headers.date
+//     );
+
+//     console.log('The data given back by the API response is:', response.data);
+//   })
+//   .catch((error) => {
+//     console.log('The value of error is:', error);
+
+//     console.log(
+//       'The value of status inside of response is:',
+//       error.response.status
+//     );
+
+//     console.log(
+//       'The data given back by the API response is:',
+//       error.response.data
+//     );
+//   });
+
+
+//////////////////////////////////
+// Verifying the Error Handling //
+//////////////////////////////////
+
+// axios.get('https://dog.ceo/api/breeds/notarealendpoint/')
+//   .then((response) => {
+//     console.log('success!');
+//   })
+//   .catch((error) => {
+//     console.log('error!', error);
+//   });
+
+
+////////////////////////
+// Using Query Params //
+////////////////////////
+
+// axios
+//   .get('https://us1.locationiq.com/v1/search.php', {
+//     params: {
+//       key: process.env['api_key'],
+//       q: 'Seattle, Washington, USA',
+//       format: 'json',
+//     },
+//   })
+//   .then((response) => {
+//     console.log('success!', response.data);
+//   })
+//   .catch((error) => {
+//     console.log('error!', error.response.data);
+//   });
+
+
+//////////////////////////////
+// Observing Asynchronosity //
+//////////////////////////////
+
+// axios.get('https://dog.ceo/api/breeds/list/all')
+//   .then(() => {
+//     console.log('success!');
+//   })
+//   .catch(() => {
+//     console.log('error!');
+//   });
+
+// console.log('I broke free on a Saturday morning');
+// console.log('I put the pedal to the floor');
+// console.log('Headed north on Mills Avenue');
+// console.log('And listened to the engine roar');
